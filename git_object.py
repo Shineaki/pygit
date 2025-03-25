f228d5d62181209bd3d3de7a429474a32c49a684
Shineaki <halmosadam92@gmail.com>
2025-03-25 21:28:35 +0100

    feat: cat

--- git_object.py
+++ git_object.py
@@ -14,7 +14,9 @@ from math import ceil
 from git_utils import repo_file
 
 
-class GitObject (object):
+class GitObject(object):
+
+    fmt = None
 
     def __init__(self, data=None):
         if data != None:
@@ -25,9 +27,9 @@ class GitObject (object):
     def serialize(self, repo):
         """This function MUST be implemented by subclasses.
 
-It must read the object's contents from self.data, a byte string, and
-do whatever it takes to convert it into a meaningful representation.
-What exactly that means depend on each subclass.
+        It must read the object's contents from self.data, a byte string, and
+        do whatever it takes to convert it into a meaningful representation.
+        What exactly that means depend on each subclass.
 
         """
         raise Exception("Unimplemented!")
@@ -37,6 +39,16 @@ What exactly that means depend on each subclass.
 
     def init(self):
         pass  # Just do nothing. This is a reasonable default!
+
+
+class GitBlob(GitObject):
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
 
 
 def object_read(repo, sha):
@@ -52,33 +64,37 @@ def object_read(repo, sha):
         raw = zlib.decompress(f.read())
 
         # Read object type
-        x = raw.find(b' ')
+        x = raw.find(b" ")
         fmt = raw[0:x]
 
         # Read and validate object size
-        y = raw.find(b'\x00', x)
+        y = raw.find(b"\x00", x)
         size = int(raw[x:y].decode("ascii"))
-        if size != len(raw)-y-1:
+        if size != len(raw) - y - 1:
             raise Exception(f"Malformed object {sha}: bad length")
 
         # Pick constructor
         match fmt:
-            case b'commit': c = GitCommit
-            case b'tree': c = GitTree
-            case b'tag': c = GitTag
-            case b'blob': c = GitBlob
+            case b"commit":
+                c = GitCommit
+            case b"tree":
+                c = GitTree
+            case b"tag":
+                c = GitTag
+            case b"blob":
+                c = GitBlob
             case _:
                 raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
 
         # Call constructor and return object
-        return c(raw[y+1:])
+        return c(raw[y + 1:])
 
 
 def object_write(obj: GitObject, repo=None):
     # Serialize object data
     data = obj.serialize()
     # Add header
-    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+    result = obj.fmt + b" " + str(len(data)).encode() + b"\x00" + data
     # Compute hash
     sha = hashlib.sha1(result).hexdigest()
 
@@ -87,7 +103,7 @@ def object_write(obj: GitObject, repo=None):
         path = repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
 
         if not os.path.exists(path):
-            with open(path, 'wb') as f:
+            with open(path, "wb") as f:
                 # Compress and write
                 f.write(zlib.compress(result))
     return sha
--- libpygit.py
+++ libpygit.py
@@ -39,13 +39,14 @@ def cmd_tag(args): pass
 def main(argv=sys.argv[1:]):
     argparser = argparse.ArgumentParser(description="The stupidest content tracker")
     argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
-    argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
-    argsp.add_argument("path",
-                       metavar="directory",
-                       nargs="?",
-                       default=".",
-                       help="Where to create the repository.")
     argsubparsers.required = True
+    argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+    argsp.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository.")
+    argsp = argsubparsers.add_parser("cat-file",
+                                     help="Provide content of repository objects")
+    argsp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"], help="Specify the type")
+
+    argsp.add_argument("object", metavar="object", help="The object to display")
     args = argparser.parse_args(argv)
     match args.command:
         case "add": cmd_add(args)
